388aa4bacefe40dda6fa7c8c40e10c4bed3e8761
Taulant Ibraj <tibraj94@gmail.com>
2020-03-23 21:11:33 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -39,18 +39,6 @@ def over?(board)
 end
 
 def winner(board)
-  index = []
-  index = won?(board)
-  if index == false
-    return nil
-  elsif board[index[0]] == "X"
-    return "X"
-  else
-    return "O"
-  end
-end
-
-def winner(board)
   if champion = won?(board)
     board[champion.first]
   end
