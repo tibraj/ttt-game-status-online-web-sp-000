f7c7d4d766f2e9d6e8fe8599fadbb47f639e6679
Taulant Ibraj <tibraj94@gmail.com>
2020-03-23 20:52:23 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -42,10 +42,10 @@ def winner(board)
   index = []
   index = won?(board)
   if index == false
-    return "X"
-  elsif board[index[0]] == "O"
-    return "O"
-  else
     return nil
+  elsif board[index[0]] == "X"
+    return "X"
+  else
+    return "O
   end
 end
