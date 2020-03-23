37b2104e9332adc96a3d7d2cc2918da178da708c
Taulant Ibraj <tibraj94@gmail.com>
2020-03-23 18:31:14 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -16,7 +16,7 @@ WIN_COMBINATIONS = [
 ]
 def won?(board)
   winning_board = []
-  if position_taken? == true
+  if position_taken?(board, index) == true
   board.include? do |i|
     WIN_COMBINATIONS[][]
   end
