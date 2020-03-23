3bec17e37a2f00257a4cd0d94009e0b2346b830e
Taulant Ibraj <tibraj94@gmail.com>
2020-03-23 18:35:55 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -15,7 +15,7 @@ WIN_COMBINATIONS = [
   [2,4,6]
 ]
 def won?(board)
-  if board = [" ", " ", " ", " ", " ", " ", " ", " ", " "
+  if board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
     false
 end
 end
